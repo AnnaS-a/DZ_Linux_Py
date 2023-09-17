2a4e8121ee4cbc3502a7dc943c0de7cdd0876af9
Анна Слукина <109862426+AnnaS-a@users.noreply.github.com>
2023-09-17 15:41:51 +0700

    Update test_positive.py

--- test_positive.py
+++ test_positive.py
@@ -1,83 +1,106 @@
+from checkers import ssh_checkout
 import yaml
-from checkers import checkout, getout
-import pytest
-
-# folderin = "/home/user/tst"
-# folderout = "/home/user/out"
-# folderext = "/home/user/folder1"
-# folderbad = "/home/user/folder2"
-
-with open('config.yaml', encoding='utf-8') as f:
-    data = yaml.safe_load(f)
+from conftest import data, save_log
+from load import upload_file
 
 
-class TestPositive:
-    def test_step1(self, after_step):
-        #test1
-        assert checkout(f'cd {data["folderin"]}; 7z a {data["folderout"]}', "Everything is Ok"), "test1 FAIL"
+def test_step0(start_time):
+    res = []
+    upload_file(data["host"], data["user"], data["password"], data["local_path"], data["remote_path"])
+    res.append(ssh_checkout(data["host"], data["user"], data["password"],
+                            f"echo {data['password']} | sudo -S dpkg -i {data['remote_path']}",
+                            "Настраивается пакет"))
+    res.append(ssh_checkout(data["host"], data["user"], data["password"],
+                            f"echo {data['password']} | sudo -S dpkg -s {data['pkgname']}",
+                            "Status: install ok installed"))
+    assert all(res), "Test Fail"
 
 
-    def test_step2(self, after_step):
-        # test2
-        assert checkout(f'cd {data["folderout"]}; 7z e {data["folderout"]}/arx2.{data["extension"]} -o{data["folderext"]} -y', "Everything is Ok"), "test2 FAIL"
+#
+#
+def test_step1(make_folders, clear_folders, make_files, start_time):
+    # create arch and ls-check that arc created
+    res1 = ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}; 7z a {}/arx1.7z".format(data['folder_in'], data['folder_out']),
+                        "Everything is Ok"), "Test1 Fail"
+    res2 = ssh_checkout(data["host"], data["user"], data["password"], "ls {}".format(data['folder_out']),
+                        "arx.7z"), "Test1 Fail"
+    save_log(start_time)
+    assert res1 and res2, "Test Fail"
 
 
-    def test_step3(self, after_step):
-        # test3
-        assert checkout(f'{data["folderout"]}; 7z t {data["folderout"]}/arx2.{data["extension"]}', "Everything is Ok"), "test3 FAIL"
-
-    # Задание 2
-    # Добавить в проект тесты, проверяющие работу команд d (удаление из архива) и u (обновление архива).
-    # Вынести в отдельные переменные пути к папкам с файлами, с архивом и с распакованными файлами.
-    # Выполнить тесты с ключом -v.
-
-
-    def test_step4(self, after_step):
-        # test4
-        assert checkout(f'cd {data["folderout"]}; 7z d arx2.{data["extension"]}', "Everything is Ok"), "test4 FAIL"
-
-
-    def test_step5(self, after_step):
-        # test4
-        assert checkout(f'cd {data["folderin"]}; 7z u {data["folderout"]}/arx2.{data["extension"]}', "Everything is Ok"), "test5 FAIL"
-
-    # Задание 4
-    # Доработать позитивные тесты таким образом, чтобы при
-    # архивации дополнительно проверялось создание файла
-    # архива, а при распаковке проверялось создание файлов.
+def test_step2(clear_folders, make_files):
+    # extract files from arc, where -o, yes -y
+    res = []
+    res.append(
+        ssh_checkout(data["host"], data["user"], data["password"],
+                     "cd {}; 7z a {}/arx1.7z".format(data['folder_in'], data['folder_out']),
+                     "Everything is Ok")), "Test2 Fail"
+    res.append(ssh_checkout(data["host"], data["user"], data["password"],
+                            "cd {}; 7z e arx1.7z -o{} -y".format(data['folder_out'], data['folder_ext']),
+                            "Everything is Ok"))
+    for item in make_files:
+        res.append(ssh_checkout(data["host"], data["user"], data["password"], "ls {}".format(data['folder_ext']), item))
+    assert all(res)
 
 
-    def test_step6(self, after_step):
-        #test6
-        res1 = checkout(f'cd {data["folderin"]}; 7z a {data["folderout"]}/arx2', "Everything is Ok")
-        res2 = checkout(f'ls {data["folderout"]}', "arx2.")
-        assert res1 and res2, "test6 FAIL"
+def test_step3():
+    # test arch
+    assert ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}; 7z t {}/arx1.7z".format(data['folder_in'], data['folder_out']),
+                        "Everything is Ok"), "Test3 Fail"
 
 
-    def test_step7(self, after_step):
-        #test7
-        res1 = checkout(f'cd {data["folderout"]}; 7z e aex2.7z -o{data["folderext"]} -y', "Everything is Ok"), "test7 FAIL"
-        res2 = checkout(f'ls {data["folderext"]}', "test1.txt")
-        res3 = checkout(f'ls {data["folderext"]}', "test2.txt")
-        assert res1 and res2 and res3, "test7 FAIL"
+def test_step4(make_folders, clear_folders, make_files):
+    # update arc
+    assert ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}; 7z u {}/arx1.7z".format(data['folder_in'], data['folder_out']),
+                        "Everything is Ok"), "Test4 Fail"
 
 
-    def test_step8(self, after_step):
-        #test8
-        assert checkout(f'cd {data["folderout"]}; 7z l arx2.{data["extension"]}', " files"), "test8 FAIL"
+def test_step5(clear_folders, make_files):
+    # what arch contain
+    res = []
+    res.append(
+        ssh_checkout(data["host"], data["user"], data["password"],
+                     "cd {}; 7z a {}/arx1.7z".format(data['folder_in'], data['folder_out']), "Everything is Ok"))
+    for item in make_files:
+        res.append(
+            ssh_checkout(data["host"], data["user"], data["password"], "cd {}; 7z l arx1.7z".format(data['folder_out']),
+                         item))
+    assert all(res)
 
 
-    def test_step9(self, after_step):
-        #test9
-        assert checkout(f'cd {data["folderout"]}; 7z x {data["folderout"]}/arx2.{data["extension"]} -y', "/home/user/out/arx2.7z")
+def test_step6(make_folders, clear_folders, make_files, make_subfolder):
+    # extract files full path
+    res = []
+    res.append(
+        ssh_checkout(data["host"], data["user"], data["password"],
+                     "cd {}; 7z a {}/arx1.7z".format(data['folder_in'], data['folder_out']), "Everything is Ok"))
+    res.append(ssh_checkout(data["host"], data["user"], data["password"],
+                            "cd {}; 7z x arx1.7z -o{} -y".format(data['folder_out'], data['folder_ext2']),
+                            "Everything is Ok"))
+    for item in make_files:
+        res.append(
+            ssh_checkout(data["host"], data["user"], data["password"], "ls {};".format(data['folder_ext2']), item))
 
-    def test_step10(self, after_step):
-        #test10
-        hash_crc32 = getout(f'cd {data["folderout"]}; crc32 arx2.{data["extension"]}').upper()
-        res1 = checkout(f'cd {data["folderout"]}; 7z h arx2.{data["extension"]}', hash_crc32.upper())
-        res2 = checkout(f'cd {data["folderout"]}; 7z h arx2.{data["extension"]}', hash_crc32.lower())
-        assert res1 or res2, "NO equal hash"
-        
+    res.append(ssh_checkout(data["host"], data["user"], data["password"], "ls {};".format(data['folder_ext2']),
+                            make_subfolder[0]))
+    res.append(ssh_checkout(data["host"], data["user"], data["password"],
+                            "ls {}/{};".format(data['folder_ext2'], make_subfolder[0]),
+                            make_subfolder[1]))  # fall in dir {}/{}
+    assert all(res)
 
-if __name__ == "__main__":
-    pytest.main(["-v"])
+
+
+def test_step7():
+    # remove arch contains
+    assert ssh_checkout(data["host"], data["user"], data["password"], "7z d {}/arx1.7z".format(data['folder_out']),
+                        "Everything is Ok"), "Test7 Fail"
+
+
+def test_step10():
+    # type of arch
+    assert ssh_checkout(data["host"], data["user"], data["password"],
+                        "7z t {}/{}".format(data['folder_out'], data['name_of_arch']),
+                        "Everything is Ok"), "Test10 Fail"

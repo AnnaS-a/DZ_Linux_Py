d6945d0626e67895a32490e3ffc23033ad3d68ef
Анна Слукина <109862426+AnnaS-a@users.noreply.github.com>
2023-09-17 15:37:18 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,80 +1,93 @@
+import random
 import string
-from random import random
-import pytest
-import yaml
-from checkers import checkout
+import subprocess
 from datetime import datetime
 
+import pytest
+import yaml
 
-with open('config.yaml', encoding='utf-8') as f:
+from checkers import checkout_positive, ssh_checkout
+
+with open('config.yaml') as f:
     data = yaml.safe_load(f)
 
 
-@pytest.fixture()      #фикстура сама создает папки(директории)
-def make_folders():
-    return checkout(f'mkdir -p {data["folderin"]} {data["folderout"]} {data["folderext"]} {data["folderext3"]} {data["folderbad"]}', "")
-
-
-@pytest.fixture()      #фикстура сама удаляет папки(директории)
-def clear_folders():
-    return checkout(f'rm -rf {data["folderin"]}/* {data["folderout"]}/* {data["folderext"]}/* {data["folderext3"]}/* {data["folderbad"]}/*', "")
-
-
-@pytest.fixture()      #фикстура сама создает файлы и хранит их в списке (произвольные названия и наполнение)
-def make_file():
-    list_of_file = []    #сгенерировали имя файла из заглавных букв и цифр, всего 5 символов
-    for i in range(data["count"]):
-        filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        if checkout("cd {}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(data["folderin"], filename), ""):
-            # если файл сгенерировался удачно, то переходим в нашу директорию создаем файл с этим именем, размер 1Мб
-            list_of_file.append(filename)
-    return list_of_file
-
-
-@pytest.fixture()      #фикстура создает каталог и файлы или возвращает созданный каталог и файлов нет, или ничего не создалось
-def make_subfolder():
-    testfilename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    subfoldername = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    if not checkout(f'cd {data["folderin"]}; mkdir {subfoldername}', ""):
-        return None, None
-    if not checkout(f'cd {data["folderin"]}/{subfoldername}; dd if=/dev/urandom of={testfilename} bs={data["bs"]} count=1 iflag=fullblock', ""):
-        return subfoldername, None
-    return subfoldername, testfilename
-
-
-@pytest.fixture()      #фикстура создает каталог fh[bd ytufnbdys[ ntcnjd
-def create_bad_archive():
-    checkout(f' cd {data["folderin"]}; 7z a {data["folderout"]}/arx2', "Everything is Ok")
-    checkout(f' cp {data["folderout"]}/arx2.{data["extension"]} {data["folderbad"]}', '')
-    checkout(f'truncate -s 1 {data["folderbad"]}/arx2.{data["extension"]}', '')
-
-
-@pytest.fixture(autouse=True)      #фикстура создает
-def speed():
-    print(datetime.now().strftime('%H:%M:%S.%f'))
-    yield
-    print(datetime.now().strftime('%H:%M:%S.%f'))
-
-# Задание 1. Дополнить проект фикстурой, которая после каждого шага теста дописывает
-# в заранее созданный файл stat.txt строку вида:
-# время, кол-во файлов из конфига, размер файла из конфига, статистика загрузки процессора
-# из файла /proc/loadavg (можно писать просто все содержимое этого файла).
+# or from conftest import data
 
 
 @pytest.fixture()
-def after_step():
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    files_count = data["count"]
-    file_size = data["bs"]
-    cpu_load_stats = _get_cpu_load_stats()
-    stat_file_path = "stat.txt"
-
-    with open(stat_file_path, "a") as file:
-        file.write(f"{timestamp}, {files_count}, {file_size}, {cpu_load_stats}\n")
+def make_folders():
+    # create all path in dir
+    return ssh_checkout(data["host"], data["user"], data["password"],
+                        "mkdir {} {} {} {} {}".format(data['folder_in'], data['folder_out'], data['folder_ext'],
+                                                      data['folder_badarx'],
+                                                      data['folder_ext2']), "")
 
 
-def _get_cpu_load_stats():
-        # получения статистики загрузки процессора из файла /proc/loadavg
-    with open("/proc/loadavg", "r") as file:
-        cpu_load_stats = file.read()
-    return cpu_load_stats
+@pytest.fixture()
+def clear_folders():
+    # remove all in dir /* all contains
+    return ssh_checkout(data["host"], data["user"], data["password"],
+                        "rm -rf {}/* {}/* {}/* {}/* {}/*".format(data['folder_in'], data['folder_out'],
+                                                                 data['folder_ext'],
+                                                                 data['folder_badarx'],
+                                                                 data['folder_ext2']), "")
+
+
+@pytest.fixture()
+def make_files():
+    # create random named(letters and numbers, length k =5) files which return as list
+    list_off_files = []
+    for i in range(data['count']):
+        filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        if ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}; dd if=/dev/urandom of={} bs={}M count=1 iflag=fullblock".format(data['folder_in'],
+                                                                                                filename,
+                                                                                                data['size_of_file']),
+                        ""):
+            list_off_files.append(filename)
+    return list_off_files
+
+
+# bs={}M size from config
+
+@pytest.fixture()
+def make_subfolder():
+    # create random named(letters and numbers, length k =5) files and folders which return as list
+    testfilename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    subfoldername = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    if not ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}; mkdir {}".format(data['folder_in'], subfoldername), ""):
+        return None, None
+    if not ssh_checkout(data["host"], data["user"], data["password"],
+                        "cd {}/{}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(data['folder_in'],
+                                                                                                  subfoldername,
+                                                                                                  testfilename), ""):
+        return subfoldername, None
+    else:
+        return subfoldername, testfilename
+
+
+@pytest.fixture()
+def make_bad_arx(make_folders, clear_folders, make_files):
+    ssh_checkout(data["host"], data["user"], data["password"],
+                 "cd {}; 7z a {}/arx1.7z".format(data['folder_in'], data['folder_badarx']),
+                 "Everything is Ok"), "Test bad Fail"
+    return ssh_checkout(data["host"], data["user"], data["password"],
+                        "truncate -s {}/badarx.7z".format(data['folder_badarx']), ""), "test FAIL"
+
+
+# fixture start time in format journalctl
+@pytest.fixture()
+def start_time():
+    return datetime.now().strftime("%Y %m %d #H:%M:%S")
+
+
+def func_test(cmd):
+    return subprocess.run(f'{cmd}', shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
+
+
+def save_log(start_time, name=data['path_journal']):
+    with open(name, 'w') as f:
+        f.write(func_test(f'sudo journalctl --since {start_time}'))
+        
